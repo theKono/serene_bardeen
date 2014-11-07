c2775d6222ab8633602a23cb7ea4aa90555fefa1
Yu Liang <yu.liang@thekono.com>
2014-11-08 07:16:53 +0800

    Create BaseApiTestCase

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -4,6 +4,7 @@
 
 # third party related imports
 import unittest2
+from webtest import TestApp
 
 # local library imports
 from serene_bardeen.models import connect_database, get_pymongo_db
@@ -18,14 +19,27 @@ class BaseDbTestCase(BaseTestCase):
 
     def setUp(self):
 
+        super(BaseDbTestCase, self).setUp()
         connect_database()
 
     def tearDown(self):
 
         self._nuke_db()
+        super(BaseDbTestCase, self).tearDown()
 
     def _nuke_db(self):
 
         db = get_pymongo_db()
-        collections = db.collection_names(include_system_collections=False)
-        map(lambda c: c.remove({}, multi=True), collections)
+        map(lambda c: db[c].remove({}, multi=True),
+            db.collection_names(include_system_collections=False))
+
+
+class BaseApiTestCase(BaseDbTestCase):
+
+    def setUp(self):
+
+        super(BaseApiTestCase, self).setUp()
+
+        if getattr(self, 'controller', None) is not None:
+            ctrl_module = __import__(self.controller, fromlist=['app'])
+            self.test_app = TestApp(ctrl_module.app)
