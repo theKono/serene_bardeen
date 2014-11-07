d668fd8b2c568f8debdcbcc2558a7c55c7204d52
Yu Liang <yu.liang@thekono.com>
2014-11-08 04:10:05 +0800

    Create config_test.py for config.py template

---
+++ serene_bardeen/config_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# standard library imports
+
+# third party related imports
+
+# local library imports
+
+
+class Config(object):
+
+    SECRET = 'abc'
+    DOMAIN_NAME = 'localhost'
+
+    DB_USERNAME = 'admin'
+    DB_PASSWORD = 'admin'
+    DB_HOST = 'localhost'
+    DB_DATABASE = 'serene_bardeen'
