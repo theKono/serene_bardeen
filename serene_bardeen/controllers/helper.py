33c690ed52a787fb3df60a4122179e8fb1c78300
Yu Liang <yu.liang@thekono.com>
2014-11-07 14:48:08 +0800

    Create controller helper

---
+++ serene_bardeen/controllers/helper.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# standard library imports
+
+# third party related imports
+import bottle
+import ujson
+
+# local library imports
+
+
+def abort(code=500, body="Unknown Error: Application stopped."):
+    """Aborts execution and causes a HTTP error."""
+
+    if isinstance(body, dict):
+        raise bottle.HTTPResponse(status=code,
+                                  content_type='application/json',
+                                  body=ujson.dumps(body, ensure_ascii=False))
+
+    bottle.abort(code, body)
+
+
+def require_parameter(key):
+
+    if key in bottle.request.json:
+        return bottle.request.json[key]
+
+    elif key in bottle.request.params:
+        return bottle.request.params[key]
+
+    else:
+        abort(400, {'message': 'Required parameter "%s" missing' % key})
