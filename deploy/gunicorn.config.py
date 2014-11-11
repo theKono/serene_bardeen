b5fa84870980eea558d8240fec4d17f116efec37
Yu Liang <yu.liang@thekono.com>
2014-11-11 14:17:26 +0800

    Add some gunicorn files

---
+++ deploy/gunicorn.config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# standard library imports
+import multiprocessing
+
+# third party related imports
+
+# local library imports
+
+
+bind = "127.0.0.1:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'eventlet'
---
+++ deploy/runit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+GUNICORN=/usr/bin/gunicorn
+ROOT=/home/ubuntu/serene_bardeen
+PID=/var/run/gunicorn.pid
+APP=main:application
+
+if [ -f $PID ]; then rm $PID; fi
+
+cd $ROOT
+exec $GUNICORN -c $ROOT/deploy/gunicorn.conf.py --pid=$PID $APP
