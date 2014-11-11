a1b1e2e1eb8368d0920f901b94262617c8b3b311
Yu Liang <yu.liang@thekono.com>
2014-11-12 01:29:35 +0800

    Add logging to gunicorn config

--- deploy/gunicorn.config.py
+++ deploy/gunicorn.config.py
@@ -10,4 +10,6 @@ import multiprocessing
 
 bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = 'eventlet'
+worker_class = 'egg:gunicorn#eventlet'
+access_logfile = '/var/log/gunicorn/access.log'
+error_logfile = '/var/log/gunicorn/error.log'
